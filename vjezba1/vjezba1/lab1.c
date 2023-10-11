d4ce61bb1e23a43d69697ec4cac83af3b10edf01
marinmesaric <mmesar00@fesb.hr>
2023-10-11 21:22:27 +0200

    Kraj predavanja

--- vjezba1/vjezba1/lab1.c
+++ vjezba1/vjezba1/lab1.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#define MAX_SIZE 50
+#define MAX_LINE 1024
+#define FILE_ERROR_OPEN -1
+
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
+
+
+int readNoRowsInFile() {
+	int cnt = 0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+
+	filePointer = fopen("students.txt", "r");
+	if (!filePointer) {
+		//printf();
+	}
+}
+
 
 int main() {
-	printf("Pozdrav svijete");
+	int noRows = 0;
+
 
 
 
 	return 0;
 }
+
